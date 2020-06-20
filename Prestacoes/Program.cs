c5ca0d1daf802702bed9a7a34ac2b3dc02372955
Marcella Carvalho <marcellapvp777@gmail.com>
2020-06-19 21:06:09 -0300

    Criação do projeto Prestações

---
+++ Prestacoes/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Prestacoes
+{
+	class Program
+	{
+		/*
+		 * 19/06/2020
+		 * A loja Mamão com Açúcar está vendendo seus produtos em 5 prestações sem juros.
+		 * O algoritmo recebe um valor de uma compra e mostra o valor das prestações.
+		 */
+
+		static void Main(string[] args)
+		{
+			decimal valor, resultado;
+			
+			Console.WriteLine("Digite o total a pagar: ");
+			valor = decimal.Parse(Console.ReadLine());
+
+			resultado = valor / 5;
+
+			Console.WriteLine("O valor total a pagar é: " + resultado);
+		}
+	}
+}
