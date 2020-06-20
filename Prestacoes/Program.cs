032300fb12ed472e2eec4b7b2ba9955906a572a1
Marcella Carvalho <marcellapvp777@gmail.com>
2020-06-19 21:12:21 -0300

    Inclusão de versionamento

--- Prestacoes/Program.cs
+++ Prestacoes/Program.cs
@@ -8,6 +8,7 @@ namespace Prestacoes
 		 * 19/06/2020
 		 * A loja Mamão com Açúcar está vendendo seus produtos em 5 prestações sem juros.
 		 * O algoritmo recebe um valor de uma compra e mostra o valor das prestações.
+		 * Versão: Beta
 		 */
 
 		static void Main(string[] args)
